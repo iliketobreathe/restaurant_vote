b6cf3dcd39f46d6b4ffe1b28d749217fe02a50e0
Aleksandr Mukhamedov <3a2nok@gmail.com>
2020-09-01 17:58:48 +0300

    ProfileRestController - added register method

--- src/main/java/ru/iliketobreathe/restaurantvote/web/user/ProfileRestController.java
+++ src/main/java/ru/iliketobreathe/restaurantvote/web/user/ProfileRestController.java
@@ -2,9 +2,14 @@ package ru.iliketobreathe.restaurantvote.web.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.iliketobreathe.restaurantvote.model.User;
 import ru.iliketobreathe.restaurantvote.to.UserTo;
+import ru.iliketobreathe.restaurantvote.util.UserUtil;
+
+import java.net.URI;
 
 import static ru.iliketobreathe.restaurantvote.web.user.SecurityUtil.authUserId;
 
@@ -28,5 +33,14 @@ public class ProfileRestController extends AbstractUserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody UserTo userTo) {
         super.update(userTo, authUserId());
+    }
+
+    @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> register(@RequestBody UserTo userTo) {
+        User created = super.create(UserUtil.createNewFromTo(userTo));
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
     }
 }
